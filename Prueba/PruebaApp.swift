cf87bc566fb8e174fe2308cab893cc3ab237b01a
David <Davidpascuallorenzo@gmail.com>
2026-04-14 12:29:15 +0200

    Initial Commit

---
+++ Prueba/PruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaApp.swift
+//  Prueba
+//
+//  Created by David Pascual Lorenzo on 14/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
