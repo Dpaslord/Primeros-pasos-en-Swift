5d3bbf6b142c6757213de16d44c920c43a44fab1
Dpaslord <Davidpascuallorenzo@gmail.com>
2026-04-15 13:59:51 +0200

    Menu para navegar entre ejercicios

--- Prueba/MenuView.swift
+++ Prueba/MenuView.swift
@@ -1,21 +1,100 @@
-//
-//  MenuView.swift
-//  Prueba
-//
-//  Created by David Pascual Lorenzo on 15/04/2026.
-//
-
 import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        NavigationStack{
-            Text("Ejercicio Hola Mundo!!")
-            Text("Ejercicio Figuras ")
-            Text("Hello, World!")
-            Text("Hello, World!")
-            Text("Hello, World!")
-
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 20) {
+                
+                Text("Menu")
+                    .font(.system(size: 30, weight: .bold))
+                    .padding(10)
+                    .background(LinearGradient(
+                        colors: [Color.cyan, Color.orange],
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    ))
+                    .foregroundStyle(Color.white)
+                    .cornerRadius(10)
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("1. ContentView")
+                        .bold()
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(LinearGradient(
+                            colors: [Color.cyan, Color.orange],
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        ))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                NavigationLink(destination: PruebaRectangulos()) {
+                    Text("2. Ejercicio de Figuras")
+                        .bold()
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(LinearGradient(
+                            colors: [Color.cyan, Color.orange],
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        ))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                NavigationLink(destination: Calculadora()) {
+                    Text("3. Calculadora")
+                        .bold()
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(LinearGradient(
+                            colors: [Color.cyan, Color.orange],
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        ))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("4. App 3")
+                        .bold()
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(LinearGradient(
+                            colors: [Color.cyan, Color.orange],
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        ))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("5. App 4")
+                        .bold()
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(LinearGradient(
+                            colors: [Color.cyan, Color.orange],
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        ))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(LinearGradient(
+                colors: [Color.blue, Color.purple],
+                startPoint: .top,
+                endPoint: .bottom
+            ))
         }
     }
 }
